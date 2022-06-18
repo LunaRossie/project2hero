755b23f9f0236b864afff0fdcfdcf79e0afdb9db
LunaRossie <>
2022-06-18 13:27:13 -0400

    adding review handlebars

--- models/index.js
+++ models/index.js
@@ -1,13 +1,27 @@
 const User = require('./User');
-const Project = require('./Project');
+const Review = require('./Review');
+const Category = require('./Category');
 
-User.hasMany(Project, {
+// Define a Driver as having many Cars, thus creating a foreign key in the `car` table
+Review.hasMany(Category, {
+  foreignKey: 'review_id',
+  onDelete: 'CASCADE',
+});
+
+// The association can also be created from the Car side
+Category.belongsTo(Review, {
+  foreignKey: 'review_id',
+});
+
+User.hasMany(Review, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+// The association can also be created from the Car side
+Review.belongsTo(User, {
+  foreignKey: 'user_id',
 });
 
-module.exports = { User, Project };
+
+module.exports = { User, Review, Category};
